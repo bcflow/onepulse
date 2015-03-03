0283497fbb5d722f2cb8926e99e164707a009f15
Cal <bcflow@riseup.net>
2015-03-03 14:28:56 -0800

    Filter icon add, sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226225700) do
+ActiveRecord::Schema.define(version: 20150303194117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,10 +104,10 @@ ActiveRecord::Schema.define(version: 20150226225700) do
     t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.text     "country"
+    t.string   "country"
     t.integer  "age"
     t.text     "gender"
-    t.text     "city"
+    t.string   "city"
     t.string   "name"
   end
 
